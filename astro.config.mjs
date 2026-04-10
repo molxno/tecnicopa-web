26239ed1e7aabba5880389d483300b375160748b
Santiago Molano <s.molano0818@gmail.com>
2026-04-10 13:18:59 -0500

    chore: add vercel plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tecnicopa.com',
   integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
